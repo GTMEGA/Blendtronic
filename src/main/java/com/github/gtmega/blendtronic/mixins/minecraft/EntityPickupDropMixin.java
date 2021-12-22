c6f90e399ac4051ac9012f222c68ca910ca54f25
basdxz <tudurap.com@gmail.com>
2021-12-22 18:40:41 +0000

    Refactor few mixins (#4)
    * Update BlockLight
    
    * Update game over mixin
    
    * Update entity pickup mixins
    
    * Update NullSafeGetBlockLightMixin.java
    
    * Update EntityPickupDropMixin.java
    
    Co-authored-by: Vitaly <vitaliilesik@gmail.com>

---
+++ src/main/java/com/github/gtmega/blendtronic/mixins/minecraft/EntityPickupDropMixin.java
@@ -0,0 +1,46 @@
+package com.github.gtmega.blendtronic.mixins.minecraft;
+
+import net.minecraft.entity.EntityLiving;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static org.objectweb.asm.Opcodes.PUTFIELD;
+
+@Mixin(EntityLiving.class)
+public abstract class EntityPickupDropMixin {
+
+    @Shadow protected abstract void dropEquipment(boolean p_82160_1_, int p_82160_2_);
+
+    /**
+     * @author FalsePattern
+     * <p>
+     * Let mobs holding loot despawn.
+     */
+    @Redirect(method = "onLivingUpdate",
+            at = @At(value = "FIELD",
+                    target = "Lnet/minecraft/entity/EntityLiving;persistenceRequired:Z",
+                    opcode = Opcodes.PUTFIELD,
+                    ordinal = 0),
+            require = 1)
+    private void bypassPersistence(EntityLiving instance, boolean value) {}
+
+    /**
+     * @author FalsePattern
+     * <p>
+     * Forces despawning entities to drop their equipment.
+     */
+    @Redirect(method = "despawnEntity",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/entity/EntityLiving;setDead()V"),
+            require = 1)
+    private void dropEquipmentOnDespawn(EntityLiving instance) {
+        ((EntityPickupDropMixin)(Object)instance).dropEquipment(false, 0);
+        instance.setDead();
+    }
+
+}
---
+++ src/main/java/com/github/gtmega/blendtronic/mixins/minecraft/GameOverFullscreenFix.java
@@ -0,0 +1,34 @@
+package com.github.gtmega.blendtronic.mixins.minecraft;
+
+import net.minecraft.client.gui.GuiGameOver;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(GuiGameOver.class)
+public class GameOverFullscreenFix {
+
+    @Shadow private int field_146347_a;
+
+    /**
+     * @author FalsePattern
+     * <p>
+     * Prevents the player from being stuck at the game over screen
+     */
+    @SuppressWarnings("rawtypes")
+    @Redirect(method = "initGui",
+            at = @At(value = "INVOKE",
+                    target = "Ljava/util/List;clear()V",
+                    ordinal = 0),
+            require = 1)
+    private void resetGui(List instance) {
+        field_146347_a = Math.max(field_146347_a, 19);
+        instance.clear();
+    }
+
+}
