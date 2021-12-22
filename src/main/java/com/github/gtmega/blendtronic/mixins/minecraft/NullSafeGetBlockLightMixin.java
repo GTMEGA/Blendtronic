0611f42101b13da8a41a3866fe3c67ca64f77191
basdxz <tudurap.com@gmail.com>
2021-12-22 18:26:49 +0000

    Update BlockLight

---
+++ src/main/java/com/github/gtmega/blendtronic/mixins/minecraft/NullSafeGetBlockLightMixin.java
@@ -0,0 +1,26 @@
+package com.github.gtmega.blendtronic.mixins.minecraft;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(World.class)
+public class NullSafeGetBlockLightMixin {
+
+    /**
+     * @author FalsePattern
+     * <p>
+     * Makes Block light valyes null safe.
+     */
+    @Redirect(method = "getBlockLightValue_do",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/world/chunk/Chunk;getBlockLightValue(IIII)I",
+                    ordinal = 0),
+            require = 1)
+    public int nullSafeGetBlockLight(Chunk chunk, int x, int y, int z, int l) {
+        return chunk == null ? 0 : chunk.getBlockLightValue(x, y, z, l);
+    }
+
+}
